ba5e6e0f7ab175a643646e9eae119b3957cefc2b
Wesley Taylor <jamsterwes@gmail.com>
2023-09-05 18:21:31 -0500

    added some basic views/controller roughly based on Linkedin

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,63 @@
 class BooksController < ApplicationController
+    # Show all
+    def index
+        @books = Book.all
+    end
+
+    # Show one
+    def show
+        @book = Book.find(params[:id])
+    end
+
+    # Actual page
+    def new
+        @book = Book.new
+    end
+
+    # Handler
+    def create
+        @book = Book.new(create_params)
+        if @book.save
+            redirect_to books_path
+        else
+            render('new')
+        end
+    end
+
+    # Handle create params
+    private def create_params
+        params.require(:book).permit(:title)
+    end
+
+    # Actual page
+    def edit
+        @book = Book.find(params[:id])
+    end
+
+    # Handler
+    def update
+        @book = Book.find(params[:id])
+        if @book.update(update_params)
+            redirect_to books_path
+        else
+            render('edit')
+        end
+    end
+
+    # Handle update params
+    private def update_params
+        params.require(:book).permit(:title)
+    end
+
+    # Actual page
+    def delete
+        @book = Book.find(params[:id])
+    end
+
+    # Handler
+    def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+        redirect_to books_path
+    end
 end
