450a7ab6d6cf3fd3a9b6e8fd2d269d3c31a3a5c6
Wesley Taylor <jamsterwes@gmail.com>
2023-09-05 19:55:38 -0500

    added new tests, accidentally changed line-endings by running git from the docker container...

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ApplicationRecord
+  # Validate that the book has a title
   validates :title, presence: true
 end
--- spec/system/creating_books_spec.rb
+++ spec/system/creating_books_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe "CreatingBooks", type: :system do
 
     # Fill out form
     fill_in 'Title', with: 'Breaking Bad'
+    # TODO: add rest
 
     # Click submit
     click_on 'Create Book'
@@ -28,13 +29,64 @@ RSpec.describe "CreatingBooks", type: :system do
   end
 
   # Rainy day
-  it 'should not create a book with invalid details' do
+  it 'should not create a book with NO title' do
     # Go to page
     visit '/books/new'
 
     # Don't! fill out form
-    # fill_in 'Title', with: 'Breaking Bad'
+    # TODO: add rest
 
+    # Click submit
+    click_on 'Create Book'
+
+    # Expect flash notice (unhappy)
+    expect(page).to have_content('Failed to create book!')
+  end
+
+  # Rainy day
+  it 'should not create a book with NO author' do
+    # Go to page
+    visit '/books/new'
+
+    # Don't! fill out form
+    fill_in 'Title', with: 'Breaking Bad'
+    fill_in 'Price', with: '0.99'
+    select '2013/09/29', from: 'Date'
+
+    # Click submit
+    click_on 'Create Book'
+
+    # Expect flash notice (unhappy)
+    expect(page).to have_content('Failed to create book!')
+  end
+
+  # Rainy day
+  it 'should not create a book with NO price' do
+    # Go to page
+    visit '/books/new'
+
+    # Don't! fill out form
+    fill_in 'Title', with: 'Breaking Bad'
+    fill_in 'Author', with: 'Vince Gilligan'
+    select '2013/09/29', from: 'Date'
+
+    # Click submit
+    click_on 'Create Book'
+
+    # Expect flash notice (unhappy)
+    expect(page).to have_content('Failed to create book!')
+  end
+
+  # Rainy day
+  it 'should not create a book with NO date' do
+    # Go to page
+    visit '/books/new'
+
+    # Don't! fill out date
+    fill_in 'Title', with: 'Breaking Bad'
+    fill_in 'Author', with: 'Vince Gilligan'
+    fill_in 'Price', with: '0.99'
+    
     # Click submit
     click_on 'Create Book'
 
